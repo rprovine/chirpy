d024ff5d5eec92c76eb1af71c03d83dd5b662e13
Reno <rprovine@gmail.com>
2023-07-17 21:57:25 -0700

    part two

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
@@ -16,7 +18,7 @@ func main() {
 		Handler: corsMux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
 
